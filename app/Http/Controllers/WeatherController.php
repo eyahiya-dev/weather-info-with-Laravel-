c1e6c695adadadef46d2e7a6194bc9f9a4687864
Yahya Mahmud <122890225+eyahiya-dev@users.noreply.github.com>
2023-11-07 06:33:53 +0600

    Initialize the project file

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class WeatherController extends Controller
+{
+    // received data from form data with post method
+    function weatherData (Request $request){
+        echo "helo";
+    }
+}
